0d4d0aad153b78606540d715e8678a82c77052fb
Ariel Schwartz <schwartz254@gmail.com>
2023-05-09 13:23:58 +0300

    Refactoring Cardlist.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,13 +5,13 @@ const CardList = ({ robots }) => {
     return (
         <div>
             {
-                robots.map((user, i) => {
+                robots.map((robot, i) => {
                     return (
                         <Card 
                             key={i} 
-                            id={robots[i].id} 
-                            name={robots[i].name} 
-                            email={robots[i].email}
+                            id={robot.id} 
+                            name={robot.name} 
+                            email={robot.email}
                         />
                     );
                 })
