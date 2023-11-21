09b6f45dae50a263c67f5b735c5edba038e3f3ff
Ariel Schwartz <schwartz254@gmail.com>
2023-11-21 10:54:20 +0200

    Incorporating redux to manage state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 import Scroll from '../components/Scroll';
 
-const App = () => {
-    const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchfield] = useState('');
+import { requestRobots, setSearchField } from "../actions";
 
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        inPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispachToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
+const App = (props) => {
+    const { searchField, onSearchChange, onRequestRobots, robots, isPending } = props;
+    
     useEffect( () => {
-        async function fetchUsers() {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
-            const users = await response.json();
-            setRobots(users);
-        }
-        fetchUsers();
+        onRequestRobots();
     }, []);
 
     const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
 
-    const handleSearchChange = (e) => {
-        setSearchfield(e.target.value);
-    }
-
-    return  robots.length === 0 
+    return  isPending 
             ? <h1>Loading...</h1>
             :   <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={handleSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots}/>
@@ -39,4 +48,4 @@ const App = () => {
                 </div>
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispachToProps)(App);
